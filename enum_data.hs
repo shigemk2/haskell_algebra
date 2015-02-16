65f820ea1d56018ccc4a6354dfb8015f05e54040
shigemk2 <i.am.shige@gmail.com>
2015-02-16 21:00:37 +0900

    コメント追加

--- enum_data.hs
+++ enum_data.hs
@@ -5,7 +5,7 @@ main = do
     print $ fromEnum Red
     print $ fromEnum Green
     print $ fromEnum White
-    print (toEnum 0 :: Color)
+    print (toEnum 0 :: Color)   -- 演算子の優先順位の関係上、::は括弧で囲む必要があります（$ではなく）
     print (toEnum 1 :: Color)
     print (toEnum 2 :: Color)
     print (toEnum 3 :: Color)
