6bc00bd978c966fe14a7f66158afb661ddf890b9
shigemk2 <i.am.shige@gmail.com>
2015-02-16 23:10:04 +0900

    mix追加

--- mix.hs
+++ mix.hs
@@ -1,11 +1,24 @@
-data Color = Magenta | Cyan | Yellow | Blue | Red | Green | White deriving Show
+data Color = Blue | Red | Magenta | Green | Cyan | Yellow | White
+    deriving Show
 
-mix Blue Red = Magenta
-mix Blue Green = Cyan
-mix Red Green = Yellow
+mix Blue    Red     = Magenta
+mix Blue    Magenta = Magenta
+mix Blue    Green   = Cyan
+mix Blue    Cyan    = Cyan
+mix Blue    Yellow  = White
+mix Red     Magenta = Magenta
+mix Red     Green   = Yellow
+mix Red     Cyan    = White
+mix Red     Yellow  = Yellow
+mix Magenta Green   = White
+mix Magenta Cyan    = White
+mix Magenta Yellow  = White
+mix Green   Cyan    = Cyan
+mix Green   Yellow  = Yellow
+mix Cyan    Yellow  = White
+mix White   _       = White
+mix c1      c2      = mix c2 c1
 
 main = do
-    print Blue
-    print $ mix Blue Red
-    print $ mix Blue Green
-    print $ mix Red Green
+    print $ mix Red Blue
+    print $ mix Red $ mix Blue Green
