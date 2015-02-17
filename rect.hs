48677552e7c0b869601d55068381d59531a9dd0a
shigemk2 <i.am.shige@gmail.com>
2015-02-17 22:02:40 +0900

    rect ベース

---
+++ rect.hs
@@ -0,0 +1,6 @@
+main = do
+    print $ contains (Rect 2 2 3 3) (Point 1 1)
+    print $ contains (Rect 2 2 3 3) (Point 2 2)
+    print $ contains (Rect 2 2 3 3) (Point 3 3)
+    print $ contains (Rect 2 2 3 3) (Point 4 4)
+    print $ contains (Rect 2 2 3 3) (Point 5 5)
