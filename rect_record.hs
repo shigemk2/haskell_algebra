a59da64a0b76dd4433fbd1e4769c31abfecef6d0
shigemk2 <i.am.shige@gmail.com>
2015-02-19 23:01:37 +0900

    rect レコード構文 修正

--- rect_record.hs
+++ rect_record.hs
@@ -1,12 +1,13 @@
 data Point = Point { px :: Int, py :: Int } deriving Show
-data Rect = Rect { x :: Int, y :: Int, w :: Int, h :: Int } deriving Show
+data Rect  = Rect  { rx :: Int, ry :: Int, rw :: Int, rh :: Int } deriving Show
 
-contains (Rect x y w h) (Point px py) =
-    x <= px && px < x + w && y <= py && py < y + h
+contains r p =
+    (rx r) <= (px p) && (px p) < (rx r) + (rw r) &&
+    (ry r) <= (py p) && (py p) < (ry r) + (rh r)
 
 main = do
-    print $ contains (Rect 2 2 3 3) (Point 1 1)
-    print $ contains (Rect 2 2 3 3) (Point 2 2)
-    print $ contains (Rect 2 2 3 3) (Point 3 3)
-    print $ contains (Rect 2 2 3 3) (Point 4 4)
-    print $ contains (Rect 2 2 3 3) (Point 5 5)
+    print $ contains Rect { rx = 2, ry = 2, rw = 3, rh = 3 } Point { px = 1, py = 1 }
+    print $ contains Rect { rx = 2, ry = 2, rw = 3, rh = 3 } Point { px = 2, py = 2 }
+    print $ contains Rect { rx = 2, ry = 2, rw = 3, rh = 3 } Point { px = 3, py = 3 }
+    print $ contains Rect { rx = 2, ry = 2, rw = 3, rh = 3 } Point { px = 4, py = 4 }
+    print $ contains Rect { rx = 2, ry = 2, rw = 3, rh = 3 } Point { px = 5, py = 5 }
